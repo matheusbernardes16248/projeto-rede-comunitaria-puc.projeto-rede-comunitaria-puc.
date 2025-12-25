926b2d74d153625cd7c53f739073d01d99b2ab79
matheusbernardes16248 <matheusnbtj@gmail.com>
2025-12-25 16:06:41 -0300

    Add files via upload
    update views

---
+++ Views/RedefinicaoSenha/RedefinirSenha.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nexumApp.Views.RedefinicaoSenha
+{
+    public class RedefinirSenhaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
